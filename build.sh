8ce9f5c00ac71ba873464c789b70addbc78dc373
obizip <ct4kuto@gmail.com>
2024-07-16 21:25:41 +0900

    rename site to docs

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cargo build --target=wasm32-unknown-unknown --release
-cp ./target/wasm32-unknown-unknown/release/wasm_dev.wasm site/
+cp ./target/wasm32-unknown-unknown/release/wasm_dev.wasm docs/
